3733d2d5f91d72e5956c720053a728b9bbfa3c79
Eugen Adamenko <adamenko.evgenyy@gmail.com>
2021-01-15 23:51:01 +0200

    added devise and active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :users
+  resources :categories do
+    resources :products
+  end
   root "pages#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210115205222_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.boolean :available, default: false, null: false
+      t.decimal :price, precision: 10, scale: 2
+
+      t.timestamps
+    end
+  end
+end
